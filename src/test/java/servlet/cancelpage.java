baf1ec5b7df3454c5d7c3d1a8062a8defccd8fcf
sanjog0206 <sanjog.poyarekar@gmail.com>
2018-07-27 21:38:06 +0530

    Update cancelpage.java

--- src/test/java/servlet/cancelpage.java
+++ src/test/java/servlet/cancelpage.java
@@ -25,9 +25,6 @@ public class cancelpage extends HttpServlet {
 					props.setProperty("ssl","true");
 					Connection connection = DriverManager.getConnection(jdbcUrl, props);
 
-					String jdbcUrl = "jdbc:postgresql://18.222.24.54:5432/postgres?user=postgres&password=&ssl=true";
-					Connection connection = DriverManager.getConnection(jdbcUrl);
-
 					Statement statement = connection.createStatement();
 					String sql= "SELECT  * FROM avt;";
 					ResultSet rs= statement.executeQuery(sql);
