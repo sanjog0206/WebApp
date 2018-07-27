a2fff768ef6b5914e57b6d924018d97b073bbc3c
sanjog0206 <sanjog.poyarekar@gmail.com>
2018-07-27 09:48:59 +0530

    Update createpage.java
    Updated IP address 18.222.24.54

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://13.58.48.167:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://18.222.24.54:5432/postgres";
 			String username="postgres";
 			String password="";
 		
